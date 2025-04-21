bf280ef31af56e3b29ba3a2c75c7191dcd53ab60
Gabor Szita <gaborszita88@gmail.com>
2025-04-21 16:34:17 -0700

    Create bigger neural network using the team_stats table

--- data_reader.py
+++ data_reader.py
@@ -5,6 +5,8 @@
 # player_stats columns: ['game_id', 'player', 'team', 'MP', 'FG', 'FGA', 'FG%', '3P', '3PA', '3P%', 'FT', 'FTA', 'FT%', 'ORB', 'DRB', 'TRB', 'AST', 'STL', 'BLK', 'TOV', 'PF', 'PTS', '+/-', 'TS%', 'eFG%', '3PAr', 'FTr', 'ORB%', 'DRB%', 'TRB%', 'AST%', 'STL%', 'BLK%', 'TOV%', 'USG%', 'ORtg', 'DRtg', 'BPM', 'PIE']
 
 import sqlite3
+import os
+import csv
 
 con = sqlite3.connect('NBA-Web-Scraper/data/NBA-Boxscore-Database.sqlite')
 cur = con.cursor()
@@ -38,6 +40,8 @@ for row in rows:
 cur.execute("SELECT * FROM game_info")
 game_info = cur.fetchall()
 
+print("Game info length: " + str(len(game_info)))
+
 cur.execute("SELECT * FROM team_stats")
 team_stats = cur.fetchall()
 
@@ -53,5 +57,38 @@ game_info_test = cur.fetchall()
 res = cur.execute("SELECT * FROM game_info")
 game_info = cur.fetchall()
 
+game_info_detailed_path = "cache_data/game_info_detailed.csv"
+
+if not os.path.exists(game_info_detailed_path):
+  # gave ChatGPT the list of fields in the team_stats table ans asked it
+  # "which of these are most useful for basketball prediction?"
+  # to select useful features
+  res = cur.execute("SELECT result, away_team, home_team, " \
+                    "ts_home.PTS, ts_home.`FG%`, ts_home.`3P%`, ts_home.`FT%`, ts_home.AST, ts_home.STL, ts_home.ORtg, ts_home.`TS%`, ts_home.STL, ts_home.BLK, ts_home.DRtg, ts_home.`DRB%`, ts_home.`eFG%`, ts_home.`TOV%`, " \
+                    "ts_away.PTS, ts_away.`FG%`, ts_away.`3P%`, ts_away.`FT%`, ts_away.AST, ts_away.STL, ts_away.ORtg, ts_away.`TS%`, ts_away.STL, ts_away.BLK, ts_away.DRtg, ts_away.`DRB%`, ts_away.`eFG%`, ts_away.`TOV%` " \
+                    "FROM game_info " \
+                    "INNER JOIN team_stats ts_home ON " \
+                    "ts_home.game_id = " \
+                    "(SELECT game_id FROM game_info game_info_2 " \
+                      "WHERE game_info_2.date < game_info.date AND (game_info_2.home_team = game_info.home_team OR  game_info_2.away_team = game_info.home_team)"\
+                      "ORDER BY date DESC LIMIT 1) " \
+                    "AND ts_home.team = game_info.home_team " \
+                    "INNER JOIN team_stats ts_away ON " \
+                    "ts_away.game_id = " \
+                    "(SELECT game_id FROM game_info game_info_2 " \
+                      "WHERE game_info_2.date < game_info.date AND (game_info_2.home_team = game_info.away_team OR  game_info_2.away_team = game_info.away_team)"\
+                      "ORDER BY date DESC LIMIT 1) " \
+                    "AND ts_away.team = game_info.away_team "
+                    "ORDER BY date ")
+  game_info_detailed = cur.fetchall()
+
+  with open(game_info_detailed_path, 'w', newline='') as file:
+    writer = csv.writer(file)
+    writer.writerows(game_info_detailed)
+
+with open(game_info_detailed_path, 'r', newline='') as file:
+  reader = csv.reader(file)
+  game_info_detailed = [[int(row[0]), row[1], row[2]] + [float(elem) for elem in row[3:]] for row in reader]
+  print("Detailed length: " + str(len(game_info_detailed)))
 
 con.close()
---
+++ test_ml_gabor_detailed.py
@@ -0,0 +1,129 @@
+import torch
+from torch.utils.data import Dataset, DataLoader
+import torch.nn.functional as F
+import torch.nn as nn
+import torch.optim as optim
+from data_reader import game_info_detailed
+import random
+
+class TeamNameMapping:
+  def __init__(self, array):
+    values = set(array)
+    self._name_to_int_mapping = {team_name: i for i, team_name in enumerate(values)}
+
+  def get_team_name_to_int_mapping(self):
+    return self._name_to_int_mapping
+
+class GameInfoDataset(Dataset):
+  def _one_hot_encode_str_array(self, array, team_name_mapping):
+    #values = set(array)
+    #name_to_int_mapping = {team_name: i for i, team_name in enumerate(values)}
+    name_to_int_mapping = team_name_mapping.get_team_name_to_int_mapping()
+    array_int = [name_to_int_mapping[i] for i in array]
+    int_tensor = torch.tensor(array_int)
+    return F.one_hot(int_tensor)
+
+  def __init__(self, data, team_name_mapping):
+    team_1 = self._one_hot_encode_str_array([elem[1] for elem in data], team_name_mapping)
+    team_2 = self._one_hot_encode_str_array([elem[2] for elem in data], team_name_mapping)
+    data_rest = torch.tensor([elem[3:] for elem in data])
+    self.features = torch.cat((team_1, team_2, data_rest), dim=1).float()
+    self.labels = torch.tensor([elem[0] for elem in data]).float()
+  
+  def __len__(self):
+    return len(self.features)
+  
+  def __getitem__(self, idx):
+    return self.features[idx], self.labels[idx]
+  
+split_idx = int(len(game_info_detailed) * 0.8)
+print(split_idx)
+
+team_name_mapping = TeamNameMapping([elem[1] for elem in game_info_detailed])
+
+train_data = game_info_detailed[:split_idx]
+test_data = game_info_detailed[split_idx:]
+
+print("Training data length: " + str(len(train_data)))
+print("Test data length: " + str(len(test_data)))
+
+train_dataset = GameInfoDataset(train_data, team_name_mapping)
+test_dataset = GameInfoDataset(test_data, team_name_mapping)
+
+batch_size = 32
+train_loader = DataLoader(train_dataset, batch_size=32)
+test_loader = DataLoader(test_dataset, batch_size=32)
+
+model = nn.Sequential(
+  nn.Linear(train_dataset[0][0].shape[0], 128),
+  nn.ReLU(),
+  nn.Linear(128, 256),
+  nn.ReLU(),
+  nn.Linear(256, 128),
+  nn.ReLU(),
+  nn.Linear(128, 1)
+)
+
+
+criterion = nn.BCEWithLogitsLoss()
+optimizer = optim.SGD(model.parameters(), lr=0.1)
+
+print("Training model")
+
+for epoch in range(300):
+  model.train()
+
+  total_loss = 0
+  num_batches = 0
+  for data in train_loader:
+    inputs, labels = data[0], data[1]
+    labels = labels.unsqueeze(1)
+    optimizer.zero_grad()
+    outputs = model(inputs)
+    loss = criterion(outputs, labels)
+    loss.backward()
+    optimizer.step()
+    total_loss += loss.item()
+    num_batches += 1
+
+  if epoch % 20 == 0:
+    print("Training Loss: " + str(total_loss/num_batches))
+
+    model.eval()
+    with torch.no_grad():
+      total_loss = 0
+      num_batches = 0
+      for data in test_loader:
+        inputs, labels = data[0], data[1]
+        labels = labels.unsqueeze(1)
+        outputs = model(inputs)
+        loss = criterion(outputs, labels)
+        total_loss += loss.item()
+        num_batches += 1
+      print("Testing loss: " + str(total_loss/num_batches))
+
+print("Baseline")
+
+def tensor_of_zeros(x):
+    return torch.zeros(x.size(0), dtype=torch.float32).unsqueeze(1)
+
+def tensor_of_ones(x):
+    return torch.ones(x.size(0), dtype=torch.float32).unsqueeze(1)
+
+total_loss = 0
+num_batches = 0
+for data in train_loader:
+  outputs = tensor_of_zeros(inputs)
+  loss = criterion(outputs, labels)
+  total_loss += loss
+  num_batches += 1
+print("All zeros loss: " + str(total_loss/num_batches))
+
+total_loss = 0
+num_batches = 0
+for data in train_loader:
+  outputs = tensor_of_ones(inputs)
+  loss = criterion(outputs, labels)
+  total_loss += loss
+  num_batches += 1
+print("All ones loss: " + str(total_loss/num_batches))
